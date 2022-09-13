bee70c6a720d2511d86b6356251b0068704485fd
JenniferKiesler <111549041+JenniferKiesler@users.noreply.github.com>
2022-09-12 19:37:13 -0500

    pseudo-code

---
+++ game.js
@@ -0,0 +1,22 @@
+// define array for choices [r, p, s]
+// define var for computerChoice
+// define var for playerChoice
+// define var for wins
+// define var for losses
+// define var for ties
+
+// prompt playerChoice
+
+// randomly choose computerChoice
+
+// compare choices if-else?
+    // if playerChoice is p and computerChoice is r
+
+// display (alert) results (won, tied, lost round)
+
+// show stats (number of wins, losses, ties)
+
+// play again? if-else
+    // if ok restart game
+    // else end game
+
